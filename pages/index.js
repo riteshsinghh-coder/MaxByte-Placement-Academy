e0d5436044d71c6028c3e0458f7b19c34282796d
riteshsinghh-coder <riteshkumar26563@gmail.com>
2024-04-20 18:32:10 +0530

    favicon

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
         <title>MaxByte Placement Academy | Best Placement Center in Faridabad | Empowering Careers </title>
         <meta name="description" content="Unlock top-notch placement services and IT courses at MaxByte Placement Academy. Discover web development, IT job placements, and career advancement opportunities in Faridabad, Haryana." />
         <meta content="Placement center in Faridabad, IT jobs, Web development courses, Career advancement, Job placements, Wipro, Infosys, Amazon, Flipkart, Google, Microsoft, Jobs in Delhi NCR, Software engineering, Coding bootcamps, Tech certifications, Internships, IT training, Employment opportunities, Technology careers, Programming courses, Technical skills, Entry-level jobs, Web design, Data science, Cloud computing, Networking, Full-stack development, UI/UX design, Mobile app development, Java, Python, JavaScript, HTML/CSS, DevOps, Artificial intelligence, Machine learning, Big data, Blockchain, Internet of Things, Virtual reality, Augmented reality, Game development, Digital marketing, Project management, Agile, Scrum, Remote work, Freelancing, Resume building, Interview preparation, Soft skills, Leadership development, Career counseling, Job placement, Talent acquisition, Staffing, Industry partnerships, Networking events, IT job fairs, Recruitment drives, Career progression, Upskilling, Reskilling, Continuing education, Lifelong learning, Curriculum, Mentorship, Alumni network." name="keywords" />
-
         <link rel="canonical" href="https://maxbyteplacementacademy.in" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <script type="application/ld+json">
   {`
     {
@@ -39,8 +39,8 @@ export default function Home() {
       ],
       "contactPoint": {
         "@type": "ContactPoint",
-        "telephone": "+1234567890",
-        "contactType": "Customer service"
+        "Phone no": "+91 7545840365",
+        "contactType": "Placement Service"
       }
     }
   `}
